ab83fd6702877dba57e78ddbbf256f1a1a475446
varghese1508 <55691245+varghese1508@users.noreply.github.com>
2023-04-22 16:30:56 +0530

    basic db setup

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 from forms import centralForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # security measure to prevent against forgery attacks
 app.config['SECRET_KEY'] = '87895ec872aed029e6073ef018ba980c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cmpName = db.Column(db.String(50), unique=True, nullable=False)
+    phoneNo = db.Column(db.String(15), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.cmpName}', '{self.phoneNo}', '{self.address}')"
 
 data = [
     {'key': 'result'}
