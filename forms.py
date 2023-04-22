752d25cbdea56e10b49d3e6fc5cf924bad2189d3
varghese1508 <55691245+varghese1508@users.noreply.github.com>
2023-04-22 17:41:53 +0530

    added addCompanyForm

--- forms.py
+++ forms.py
@@ -7,3 +7,13 @@ class centralForm(FlaskForm):
     cmpname = StringField('CmpName', validators=[DataRequired(), 
                                                     Length(min=2, max=50)])
     submit = SubmitField ('Search') # param is just the display label
+
+class addCompanyForm(FlaskForm):
+    # cmpName -> Company Name
+    cmpName = StringField('cmpName', validators=[DataRequired(), 
+                                                    Length(min=2, max=50)])
+    phoneNo = StringField('phoneNo', validators=[DataRequired(), 
+                                                    Length(min=1, max=15)])
+    address = StringField('address', validators=[DataRequired(), 
+                                                    Length(min=1, max=100)])
+    submit = SubmitField ('Search') # param is just the display label
