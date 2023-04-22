d6fbeb7d2cf5016890d341e97434f46b45ec8b42
varghese1508 <55691245+varghese1508@users.noreply.github.com>
2023-04-22 18:15:14 +0530

    adding forms

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from forms import centralForm
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import centralForm, addCompanyForm
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,8 +9,7 @@ app.config['SECRET_KEY'] = '87895ec872aed029e6073ef018ba980c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-
-app.app_context().push()
+app.app_context().push()    # required for db commands for flask in python
 
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,8 +18,9 @@ class Company(db.Model):
     address = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.cmpName}', '{self.phoneNo}', '{self.address}')"
+        return f"Cmp('{self.cmpName}', '{self.phoneNo}', '{self.address}')"
 
+# dummy Data: ;to remove
 data = [
     {'key': 'result'}
 ]
@@ -33,6 +33,23 @@ def homeFun():
 def queryFun():
     form = centralForm()
     return render_template('query.html', form=form)
+
+@app.route("/modDB")
+def modDBfun():
+    form = addCompanyForm()
+
+    if form.validate_on_submit():
+        newCmp = Company(cmpName=form.cmpName.data,
+                            phoneNo=form.phoneNo.data,
+                            address=form.address.data
+                            )
+        db.session.add(newCmp)
+        db.session.commit()
+        flash(f'{newCmp.cmpName} added to database')
+
+        return redirect(url_for('modDB'))
+
+    return render_template('modDB.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
